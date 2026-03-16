cf73a0a38cce35f5e2fddf6c9bf66facd95733e4
Noorulain-annie <noorannie42501@gmail.com>
2026-03-16 12:52:53 +0500

    initilized

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-sm font-bold text-blue-500">
+        Tailwind + React + Vite 8 Works 🚀
+      </h1>
+    </div>
+  );
+}
